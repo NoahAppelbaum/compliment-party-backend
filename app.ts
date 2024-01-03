c1257800b4bc9124036d37ccd0366c26193760ff
Noah Appelbaum <noahappelbaum@gmail.com>
2024-01-03 17:05:53 -0600

    server boilerplate

---
+++ app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+export default app;
+
+/*
+TODO: Well it seems like I need a router --
+  a home page: make game and start WS room
+  and
+  a party room -- connected to WS and placed in room
+
+  buuut if I'm working in React, then... hm. Is this a usecase for
+    next.js?
+*/
---
+++ server.ts
@@ -0,0 +1,6 @@
+import app from "./app";
+const PORT = 3000;
+
+app.listen(PORT, function () {
+  console.log(`Started http://localhost:${PORT}/`);
+})
